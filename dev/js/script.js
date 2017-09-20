0d543580059ddd9e3111b8a492bdd2edfb813fa1
kabatskayaolga <kabatskaya.olga@gmail.com>
2017-09-20 12:56:43 +0300

    changes 20.09 -3

--- dev/js/script.js
+++ dev/js/script.js
@@ -4,41 +4,14 @@ jQuery(document).ready(function($){
 	});
 
 
-	$('.slider_block').slick({
+	$('.about_slider').slick({
 		infinite: true,
 		speed: 300,
 		dots: true,
 		// slidesToShow: 3,
 		// centerMode: true,
 		variableWidth: true
-	//   responsive: [
-	//     {
-	//       breakpoint: 1024,
-	//       settings: {
-	//         slidesToShow: 3,
-	//         slidesToScroll: 3,
-	//         infinite: true,
-	//         dots: true
-	//       }
-	//     },
-	//     {
-	//       breakpoint: 600,
-	//       settings: {
-	//         slidesToShow: 2,
-	//         slidesToScroll: 2
-	//       }
-	//     },
-	//     {
-	//       breakpoint: 480,
-	//       settings: {
-	//         slidesToShow: 1,
-	//         slidesToScroll: 1
-	//       }
-	//     }
-	//     You can unslick at a given breakpoint now by adding:
-	//     settings: "unslick"
-	//     instead of a settings object
-	//   ]
+	
 	}).magnificPopup({
 		delegate: 'a',
 		type: 'image',
@@ -61,5 +34,36 @@ jQuery(document).ready(function($){
 		}
 		
 	});
+
+	$('.lecturers_slider').slick({
+		infinite: true,
+		speed: 300,
+		dots: true,
+		slidesToShow: 5,
+		responsive: [
+		    {
+		      breakpoint: 1200,
+		      settings: {
+		        slidesToShow: 3,
+		      }
+		    },
+		    {
+		      breakpoint: 840,
+		      settings: {
+		        slidesToShow: 2,
+		      }
+		    },
+		    {
+		      breakpoint: 700,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		    // You can unslick at a given breakpoint now by adding:
+		    // settings: "unslick"
+		    // instead of a settings object
+		  ]
+	})
 });
 
