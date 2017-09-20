d3cb31a18e1901b4c015be02e620b2061fa1a28d
kabatskayaolga <kabatskaya.olga@gmail.com>
2017-09-20 17:18:49 +0300

    changes 20.09 -3

--- dev/js/script.js
+++ dev/js/script.js
@@ -40,9 +40,10 @@ jQuery(document).ready(function($){
 		speed: 300,
 		dots: true,
 		slidesToShow: 5,
+		adaptiveHeight: true,
 		responsive: [
 		    {
-		      breakpoint: 1200,
+		      breakpoint: 1235,
 		      settings: {
 		        slidesToShow: 3,
 		      }
@@ -60,10 +61,122 @@ jQuery(document).ready(function($){
 		        slidesToScroll: 1
 		      }
 		    }
-		    // You can unslick at a given breakpoint now by adding:
-		    // settings: "unslick"
-		    // instead of a settings object
+		 
 		  ]
-	})
+	});
+
+
+	$('.lessons_slider').slick({
+		infinite: true,
+		speed: 300,
+		dots: true,
+		arrows: false,
+		// slidesToShow: 1,		
+	}).magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+
+
+
+	$('.reviews_slider').slick({
+		infinite: true,
+		speed: 300,
+		dots: false,
+		slidesToShow: 6,
+		// slidesToShow: 1,	
+		responsive: [
+		    {
+		      breakpoint: 1235,
+		      settings: {
+		        slidesToShow: 4,
+		      }
+		    },
+		    {
+		      breakpoint: 840,
+		      settings: {
+		        slidesToShow: 3,
+		      }
+		    },
+		    {
+		      breakpoint: 700,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 2
+		      }
+		    }
+		 
+		  ]	
+	}).magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+
+
+	$('.partners_slider').slick({
+		infinite: true,
+		speed: 300,
+		dots: false,
+		slidesToShow: 8,
+		responsive: [
+		    {
+		      breakpoint: 1235,
+		      settings: {
+		        slidesToShow: 6,
+		      }
+		    },
+		    {
+		      breakpoint: 840,
+		      settings: {
+		        slidesToShow: 4,
+		      }
+		    },
+		    {
+		      breakpoint: 700,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 1
+		      }
+		    }
+		 
+		  ]
+		// slidesToShow: 1,		
+	});
 });
 
